90106d6f9e0da7381001cff93159f07e0aad6e9a
Curtis Sanderson <55060423+curtis2101@users.noreply.github.com>
2023-08-14 15:29:48 +0200

    added themes, colors and typography

---
+++ lib/theme/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class FondueSwapColor {
+  static const Color midnightBlack = Color.fromRGBO(0, 0, 0, 1);
+  static const Color graphite = Color.fromRGBO(28, 29, 33, 1);
+  static const Color mistyLavender = Color.fromRGBO(228, 228, 240, 1);
+  static const Color goldenSunset = Color.fromRGBO(255, 168, 48, 1);
+  static const Color deepAuburn = Color.fromRGBO(128, 84, 24, 1);
+  static const Color cherryRed = Color.fromRGBO(255, 0, 0, 1);
+  static const Color stormyNight = Color.fromRGBO(39, 42, 48, 1);
+  static const Color forestGreen = Color.fromRGBO(0, 143, 58, 1);
+}
