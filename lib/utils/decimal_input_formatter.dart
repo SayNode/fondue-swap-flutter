27ca5010dffe35de2d0718c8511bfea6a9a4ce4e
Curtis Sanderson <55060423+curtis2101@users.noreply.github.com>
2023-08-14 15:43:30 +0200

    added utils

---
+++ lib/utils/decimal_input_formatter.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/services.dart';
+import 'dart:math' as math;
+
+class DecimalTextInputFormatter extends TextInputFormatter {
+  DecimalTextInputFormatter(this.decimalRange);
+
+  final int decimalRange;
+
+  @override
+  TextEditingValue formatEditUpdate(
+    TextEditingValue oldValue,
+    TextEditingValue newValue,
+  ) {
+    TextSelection newSelection = newValue.selection;
+
+    String value = newValue.text;
+    value = value.replaceAll(',', '.');
+    String truncated = value;
+    if (value.contains(".") &&
+        value.substring(value.indexOf(".") + 1).length > decimalRange) {
+      truncated = oldValue.text;
+      newSelection = oldValue.selection;
+    } else if (value == ".") {
+      truncated = "0.";
+
+      newSelection = newValue.selection.copyWith(
+        baseOffset: math.min(truncated.length, truncated.length + 1),
+        extentOffset: math.min(truncated.length, truncated.length + 1),
+      );
+    } else if ('.'.allMatches(value).length > 1) {
+      truncated = oldValue.text;
+      newSelection = oldValue.selection;
+    } else if (double.tryParse(value) == null&&value.isNotEmpty) {
+      truncated = oldValue.text;
+      newSelection = oldValue.selection;
+    }
+    return TextEditingValue(
+      text: truncated,
+      selection: newSelection,
+      composing: TextRange.empty,
+    );
+  }
+}
