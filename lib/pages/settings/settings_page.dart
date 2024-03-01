c13094ce11a510d186b0bee9a27e0520da7f0d3d
yann <yann@saynode.ch>
2024-03-01 09:27:02 +0100

    removed debug text

--- lib/pages/settings/settings_page.dart
+++ lib/pages/settings/settings_page.dart
@@ -1,29 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-
-import '../../utils/globals.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: <Widget>[
-          if (devMode)
-            Obx(
-              () {
-                return Text(
-                  'Debug messages: \n ${prodDebugMessage.value}',
-                  style: const TextStyle(color: Colors.red),
-                );
-              },
-            )
-          else
-            const SizedBox(),
-        ],
-      ),
+    return const SingleChildScrollView(
+      child: Column(),
     );
   }
 }
